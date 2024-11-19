28f2d612d204f1c6339be5e8f7fdf4ca732ead34
Andreea <andreea.celmare@gmail.com>
2024-11-19 15:39:23 +0100

    first commit

---
+++ src/main/java/com/example/Page_Craft_Java_JPA/PageCraftJavaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Page_Craft_Java_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageCraftJavaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageCraftJavaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Page_Craft_Java_JPA/PageCraftJavaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Page_Craft_Java_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageCraftJavaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
